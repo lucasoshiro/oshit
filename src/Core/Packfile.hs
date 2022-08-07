b8d907b98cbc3155ad6ff1a6bda58567f1f280db
Lucas Oshiro <lucasseikioshiro@gmail.com>
2022-08-07 20:44:14 -0300

    wip

--- src/Core/Packfile.hs
+++ src/Core/Packfile.hs
@@ -87,6 +87,9 @@ parseIdxFile path = do
       | o <- offs
       ]
 
+firstBit :: Char -> Bool
+firstBit = (== 0x80) . (.&. 0x80) . ord
+
 parsePackFile :: FilePath -> IO B.ByteString
 parsePackFile path = do
   packFile <- B.readFile path
@@ -101,28 +104,106 @@ parsePackFile path = do
 getObjectData :: Idx -> B.ByteString -> Hash -> Maybe PackObj
 getObjectData idx packfile hash = Map.lookup hash idx >>= getObjectInOffset packfile
 
+getSizeHeader :: B.ByteString -> String
+getSizeHeader b = if firstBit . B.head $ b
+                  then (B.head b) : (getSizeHeader . B.tail $ b)
+                  else [B.head b]
+
 getObjectInOffset :: B.ByteString -> Int -> Maybe PackObj
 getObjectInOffset packfile offset = do
-  let firstBit :: Char -> Bool
-      firstBit = (== 0x80) . (.&. 0x80) . ord
-      
-      getHeader :: B.ByteString -> String
-      getHeader b = if firstBit . B.head $ b
-                    then (B.head b) : (getHeader . B.tail $ b)
-                    else [B.head b]
-
-      dataStart = B.drop (offset - 0xc) packfile
-      header = getHeader dataStart
+  let dataStart = B.drop (offset - 0xc) packfile
+      header = getSizeHeader dataStart
       objType' = getPackObjType . (flip shift $ (-4)) . (0x70 .&. ) . ord . head $ header
       headerSize = length header
       size = parseSize . B.pack $ header
-      content = decompress $ B.drop headerSize dataStart
 
   objType <- objType'
+
+  content <- case objType of
+    PackOfsDelta -> fail "ofs not implemented yet"
+    PackRefDelta -> fail "ref not implemented yet"
+    _ -> return . decompress $ B.drop headerSize dataStart
 
   -- non-deltified only
   return (objType, size, content)
   
+readRefDelta :: B.ByteString -> Int -> IO (B.ByteString, PackObjType)
+readRefDelta bs size = do
+  let hash' = B.take 20 bs
+      hash = B16.encode hash'
+      remaining = B.drop 20 bs
+
+  baseObj <- searchInPackFiles $ hash
+
+  readDelta remaining baseObj (size - 20)
+  
+readDelta :: B.ByteString -> Maybe PackObj -> Int -> IO (B.ByteString, PackObjType)
+readDelta bs baseObj size' = do
+  (objType, _, baseData) <- case baseObj of
+    (Just o) -> return o
+    Nothing  -> fail "object not found in packfiles"
+
+  let stream = decompress bs
+
+      baseSize' = getSizeHeader stream
+      objSize' = getSizeHeader
+        . B.drop (length baseSize')
+        . B.pack
+        $ baseSize'
+
+      baseSize = parseSize . B.pack $ baseSize'
+      objSize = parseSize . B.pack $ objSize'
+
+      size = size' - (length baseSize' + length objSize')
+      remaining = B.take size
+        . B.drop (length baseSize' + length objSize')
+        $ bs
+
+  return (applyDeltaInstructions baseData remaining, objType)
+
+applyDeltaInstructions :: B.ByteString -> B.ByteString -> B.ByteString
+applyDeltaInstructions base bs = B.empty
+  where fstBit = firstBit . B.head $ bs
+        
+
+applyData :: B.ByteString -> B.ByteString -> (B.ByteString, B.ByteString)
+applyData input bs = (output, remaining)
+  where size = (.&. 0x7f) . ord . B.head $ bs
+        data' = B.take size . B.drop 1 $ bs
+        output = B.pack $ B.unpack input ++ B.unpack data'
+        remaining = B.drop (size + 1) bs
+
+applyCopy :: B.ByteString -> B.ByteString -> B.ByteString -> (B.ByteString, B.ByteString)
+applyCopy input bs base = (output, remaining)
+  where (off, dataSize, size) = readOffsetAndSize bs
+        copied = B.take dataSize . B.drop off $ base
+        output = B.pack $ B.unpack input ++ B.unpack copied
+        remaining = B.drop size bs
+
+readOffsetAndSize :: B.ByteString -> (Int, Int, Int)
+readOffsetAndSize bs = (off, dataSize, size)
+  where fstBits = byteToBoolList . B.head $ bs
+        offBits = reverse . take 3 . drop 4 $ fstBits
+        dataSizeBits = reverse . take 4 $ fstBits
+
+        nOff = sum [if b then 1 else 0 | b <- offBits]
+        nObjSize = sum [if b then 1 else 0 | b <- dataSizeBits]
+
+        offBytes = take nOff . drop 1 . B.unpack $ bs
+        dataSizeBytes = take nObjSize . drop (nOff + 1) . B.unpack $ bs
+
+        readWeirdFormat :: [Bool] -> [Char] -> [Char]
+        readWeirdFormat [] _ = []
+        readWeirdFormat (True:bits) (c:cs) = c : (readWeirdFormat bits cs)
+        readWeirdFormat (False:bits) cs = '\0' : (readWeirdFormat bits cs)
+
+        offExpanded = readWeirdFormat offBits offBytes
+        dataSizeExpanded = readWeirdFormat dataSizeBits dataSizeBytes
+
+        off = foldl1 (\ac b -> ac * 256 + b) $ map ord offExpanded
+        dataSize = foldl1 (\ac b -> ac * 256 + b) $ map ord dataSizeBytes
+
+        size = 1 + nOff + nObjSize
 
 idxPath :: Hash -> String
 idxPath hash = concat [".git/objects/pack/pack-", B.unpack hash, ".idx"]
