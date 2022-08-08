31f18e1cb05e3efb24a20c9261d1c557b4e30d4a
d3adb5 <me@d3adb5.net>
2022-08-07 22:34:14 -0300

    test: use quickcheck for automated testing
    Use QuickCheck to automate testing inputs, provided we write generators
    for things that matter and import them when they already exist.

--- test/Core/ReflogSpec.hs
+++ test/Core/ReflogSpec.hs
@@ -1,31 +1,23 @@
 module Core.ReflogSpec (spec) where
 
 import Test.Hspec
+import Test.Hspec.QuickCheck
+import Test.Oshit ()
 
-import Data.ByteString.Char8 (unpack)
+import Data.Time.LocalTime (zonedTimeToUTC)
 
 import Core.Reflog
-
-exampleReflogEntryStr :: String
-exampleReflogEntryStr =
-  "9801739daae44ec5293d4e1f53d3f4d2d426d91c\
-  \ 7ef021a2344b022be53f36a88758612924d94163\
-  \ Example Author <author@email.com>\
-  \ 1657669218 -0300\ttest: an example title"
 
 spec :: Spec
 spec = do
   describe "ReflogEntry" $ do
-    let reflogEntry = read exampleReflogEntryStr
-        stringEntry = show reflogEntry
-
-    it "is properly read from string" $ do
-      unpack (oldHash reflogEntry) `shouldBe` "9801739daae44ec5293d4e1f53d3f4d2d426d91c"
-      unpack (newHash reflogEntry) `shouldBe` "7ef021a2344b022be53f36a88758612924d94163"
-      author reflogEntry `shouldBe` "Example Author"
-      email reflogEntry `shouldBe` "author@email.com"
-      title reflogEntry `shouldBe` "test: an example title"
-      show (timestamp reflogEntry) `shouldBe` "2022-07-12 20:40:18 -0300"
-
-    it "is properly converted to string" $ do
-      stringEntry `shouldBe` exampleReflogEntryStr
+    prop "has an isomorphism in read/show" $ \entry -> do
+      let readEntry = read $ show entry
+          timestamp' = zonedTimeToUTC . timestamp
+      oldHash readEntry    `shouldBe` oldHash entry
+      newHash readEntry    `shouldBe` newHash entry
+      author readEntry     `shouldBe` author entry
+      email readEntry      `shouldBe` email entry
+      title readEntry      `shouldBe` title entry
+      timestamp' readEntry `shouldBe` timestamp' entry
+      show readEntry       `shouldBe` show entry
---
+++ test/Test/Oshit.hs
@@ -0,0 +1,70 @@
+module Test.Oshit where
+
+-- -----------------------------------------------------------------------------
+-- |
+-- Module      : Test.Oshit
+-- Description : Instances of the Arbitrary typeclass and some generators.
+--
+-- This module contains oprhan instances of the Test.QuickCheck.Arbitrary
+-- typeclass as well as some useful generators for things such as author names,
+-- emails, SHA1 hashes, etc.
+--
+-- Its purpose is to expose symbols to other test modules.
+-- -----------------------------------------------------------------------------
+
+import Test.QuickCheck
+import Test.QuickCheck.Instances.Time ()
+
+import qualified ASCII.Char as ASCII
+import ASCII.Lists
+import Data.ByteString.Char8 (pack)
+import Data.Char (chr)
+import Data.Fixed (Pico)
+import Data.Time.Lens (modL, seconds)
+import Text.Printf (printf)
+
+import Core.Reflog (ReflogEntry(..))
+
+-- | Orphan instance of MonadFail for Test.QuickCheck.Gen, for pattern matching.
+instance MonadFail Gen where
+  fail = error
+
+-- | Generate a random String from a provided subset of the ASCII table.
+asciiGen :: [ASCII.Char] -> Int -> Gen String
+asciiGen s n = vectorOf n . elements $ map (chr . ASCII.toInt) s
+
+-- | Generate a random SHA1 hash.
+sha1Gen :: Gen String
+sha1Gen = asciiGen hexDigits 40
+
+-- | Generate a random author name with up to 4 words.
+authorGen :: Gen String
+authorGen = chooseInt (2, 4) >>= \names ->
+  fmap unwords . vectorOf names . sized $ asciiGen letters . (+ 1)
+
+-- | Generate a random email address.
+emailGen :: Gen String
+emailGen = do
+  let smallNameGen = sized $ asciiGen smallLetters . (+ 1)
+      emailFieldsGen = vectorOf 3 smallNameGen
+  [u, d, t] <- emailFieldsGen
+  return $ printf "%s@%s.%s" u d t
+
+-- | Mathematical floor that applies to a Pico value. Here to force Int range
+-- constraints and avoid the type-defaults warning.
+picoFloor :: Pico -> Pico
+picoFloor = (fromIntegral :: Int -> Pico) . floor
+
+-- | Generate a random ReflogEntry by instantiating Arbitrary. Due to Git's time
+-- format, the randomly generated timestamp has its seconds floor'd.
+instance Arbitrary ReflogEntry where
+  arbitrary = do
+    [oldH, newH] <- vectorOf 2 $ fmap pack sha1Gen
+    au <- authorGen
+    em <- emailGen
+    st <- modL seconds picoFloor <$> arbitrary
+    tt <- sized $ asciiGen printableCharacters . (+ 1)
+    return ReflogEntry { oldHash = oldH, newHash = newH
+                       , author = au, email = em
+                       , timestamp = st, title = tt
+                       }
