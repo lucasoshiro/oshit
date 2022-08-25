6b8b40db19ae11919656a7ce952796b13ffdc46e
d3adb5 <me@d3adb5.net>
2022-08-24 21:45:14 -0300

    test: test ErrorCall when reading from invalid str
    Test that trying to read a FileMode from an invalid string will yield an
    exception of type ErrorCall.

--- test/Core/CoreSpec.hs
+++ test/Core/CoreSpec.hs
@@ -4,6 +4,8 @@ import Test.Hspec
 import Test.Hspec.QuickCheck
 import Test.Oshit ()
 
+import Control.Exception (evaluate)
+
 import Core.Core
 
 spec :: Spec
@@ -11,3 +13,7 @@ spec = do
   describe "FileMode" $ do
     prop "has an isomorphism in read/show" $ \fm -> do
       read (show fm) `shouldBe` (fm :: FileMode)
+
+    context "when read from an invalid string" $ do
+      it "throws an ErrorCall exception" $ do
+        evaluate (read "123456" :: FileMode) `shouldThrow` anyErrorCall
