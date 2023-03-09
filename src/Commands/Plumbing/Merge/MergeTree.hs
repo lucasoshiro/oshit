93b5c6008210623970dbef81c0a33a2a690e450a
Lucas Oshiro <lucasseikioshiro@gmail.com>
2023-03-09 10:01:11 -0300

    merge-tree: remove code duplication

--- src/Commands/Plumbing/Merge/MergeTree.hs
+++ src/Commands/Plumbing/Merge/MergeTree.hs
@@ -8,10 +8,8 @@ import qualified Data.ByteString.Char8      as B
 
 cmdMergeTree :: Command
 cmdMergeTree (o:a:b:_) = do
-  let (hashO, hashA, hashB) = (B.pack o, B.pack a, B.pack b)
-  let treeO = loadObject hashO
-  let treeA = loadObject hashA
-  let treeB = loadObject hashB
+  let (treeO, treeA, treeB) = (hashToTree o, hashToTree a, hashToTree b)
+      hashToTree = loadObject . B.pack
 
   merged <- mergeTree treeO treeA treeB
   putStrLn $ B.unpack . hashObject $ merged
